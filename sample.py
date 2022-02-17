820d998f4dcf91a5ffc92dc926ac935d645b0fa6
Waleed Allaudin <87147415+wal238@users.noreply.github.com>
2022-02-16 22:10:51 -0500

    Update and rename foo.py to sample.py

---
+++ sample.py
@@ -0,0 +1,22 @@
+
+"""
+Helper functions live here
+"""
+
+
+def increment_by_two(var_to_increment):
+    """
+    This function increments input by two
+    :param var_to_increment: param to increment
+    :return: incremented variable
+    """
+    return var_to_increment + 2
+
+
+def increment_by_three(var_to_increment):
+    """
+    This function increments input by three
+    :param var_to_increment: param to increment
+    :return: incremented variable
+    """
+    return var_to_increment + 3
